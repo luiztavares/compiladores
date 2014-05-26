0deee1f674761e9693cef75f23c80f913794b4fa
luiztavares <luiz_andre92@hotmail.com>
2014-05-26 17:38:06 -0300

    first commit

---
+++ nossos_arquivos/entrada.cc
@@ -0,0 +1,51 @@
+??teste
+??[ 3 : 6.8 [
+
+??[3.2: 7]
+
+??< 3, 7 >decimal minhaVar = {4, 5.7, -1, "oi"}, a = 7 , b, c= 8;
+
+??bool a =  verdadeiro;
+
+??caractere _____________________ = {"foo", "oi", "you"}, a = {"foo"};
+
+??a = 14;b = 666;
+??texxt = "disuahdiajshdiuashdiasud";
+
+
+A = 123+ 3123123 - 782168479 em [13:123[;
+para i em [5:32] incremente -2 :;
+se 7 mod 2 igual 0 entao
+	string = "se fudeu menache";
+senao
+	string  = "tabom c tava certo";
+	
+se i < temp entao ;
+senao
+	a = a + 1;
+	
+	
+se 2 entao
+	ok
+	a = 8;
+	b = 2 + 1;
+	ko
+senao
+	ok
+	
+	c = verdadeiro;
+	ko
+	
+enquanto 2 + 2 igual 5 :
+ok
+	string = "la na minha casa";
+	string = "radiohead";
+ko
+
+
+repita 
+ok
+	a = a+1;
+	b = b + 1;
+ko
+ate a < 0 ;	
